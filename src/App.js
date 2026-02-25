0c90005ae23040b0d484ee2716813969f290fe88
jamellknows <youremail@example.com>
2026-02-25 00:06:12 +0000

    Starting the 24th day changes.

--- src/App.js
+++ src/App.js
@@ -2789,7 +2789,7 @@ const rightGrid = [
   },
   {
     id: 2,
-    title: "Sea villages, sky turtles (beyblades, castles) and Africa and the Caribbean.",
+    title: "Sea villages, sky turtles (beyblades, castles).",
     frontImage: "/images/seaandsky.png",
     backImage: "./images/card-back.jpg",
     content: "",
